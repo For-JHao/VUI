eff1f9ca1ec8dc821cc08d3483ddd8159eabc5ea
JHao <985206133@qq.com>
2021-05-09 15:36:28 +0800

    switch 1.0
    init

--- src/components/switch.vue
+++ src/components/switch.vue
@@ -1,15 +1,54 @@
 <template>
   <div>
-      <label class="switch-label">
-          <span class="switch-border"></span>
-          <span class="switch-button"></span>
+      <label 
+      :class='["switch-label",{"switch-label-on":value}]' 
+      @click="switchChange"
+      ref="swLabel">
+          <span :class='["switch-core",{"switch-core-on":value}]'></span>
       </label>
   </div>
 </template>
 
 <script>
 export default {
-    name:"jh-switch"
+    name:"jh-switch",
+    props:{
+        value:{
+            type:Boolean,
+            default:false
+        },
+        onColor:{
+            type:String,
+            default:''
+        },
+        offColor:{
+            type:String,
+            dafalut:''
+        }
+    },
+    methods:{
+        //switch的v-model实现
+        switchChange(){
+            this.$emit('input',!this.value);
+        },
+        //用户自定义颜色实现
+        setColor(){
+            if(this.onColor||this.offColor){
+                let style=this.$refs.swLabel.style;
+                let color=this.value? this.onColor:this.offColor
+                style.backgroundColor=color;
+                style.borderColor=color;
+            }
+        }
+    },
+    watch:{
+        value(){
+            this.setColor()
+        }
+    },
+    mounted(){
+        this.setColor()
+    }
 }
 </script>
 
@@ -21,16 +60,25 @@ export default {
     border: 1.5px solid black;
     border-radius: 8px;
     position: relative;
+    transition: all 0.35s ease;
 }
-.switch-border{
+.switch-core{
     display: inline-block;
-    width: 7px;
-    height: 7px;
+    width: 8px;
+    height: 8px;
+    background-color: white;
     border: 1.5px solid black;
     border-radius: 6px;
     position: absolute;
     top: -1px;
     left: -1px;
+    transition: all 0.35s ease;
 }
-
+.switch-core-on{
+    left: 21px;
+    background-color: rgb(53, 52, 52);
+}
+.switch-label-on{
+    background-color: rgb(189, 187, 185);
+}
 </style>
